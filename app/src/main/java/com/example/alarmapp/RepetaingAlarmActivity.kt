3e7f592b24110f6a802233bdfd5d27ffcd9bf9ff
Dzikstarts <imam@fahru.my.id>
2022-01-17 15:10:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/alarmapp/RepetaingAlarmActivity.kt
@@ -0,0 +1,11 @@
+package com.example.alarmapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RepetaingAlarmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_repetaing_alarm)
+    }
+}
